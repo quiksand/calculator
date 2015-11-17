72fa65e40848fd470baaf6e1eeab8dbb56e31182
Cody Schindler <admin@codyschindler.com>
2015-11-16 23:11:44 -0800

    Added back buttons, code cleanup

--- calculator-js.js
+++ calculator-js.js
@@ -2,17 +2,19 @@
  * FILENAME:    calculator-js.js
  * DESCRIPTION: This is the javascript for codyschindler.com/fcc/calculator.
  * REQS:        jQuery
- * VERISON:     1.0.1
- * UPDATED:     2015-11-09
+ * VERISON:     1.0.2
+ * UPDATED:     2015-11-16
  * WRITTEN BY:  Cody Schindler (quiksand)
  * ORGANIZATION: n/a
  * CHANGELOG:  
  * 1.0.0 (2015-11-03): Creation
- * 1.0.1 (2015-11-09): Deployment
+ * 1.0.1 (2015-11-09): Deployment-nevermind
+ * 1.0.2 (2015-11-12): Started from scratch on JS. Much cleaner now.
+ * 1.0.3 (2015-11-16): Deploym
 **/
 //TODO: 
-//case first = when on regOne causes "= 0" rather than "0 = 0"
-
+//Floating point erros can occur on division and modulo, although most should be rounded out. Should be fixed for other ops.
+//Display overflow not handled properly. Want similar results to display:rtl, but that screws up handling of negative numbers. Oh well, time to ship and move on to other projects. 
 
 $(document).ready(function(){
 	var displayVar = "";
@@ -21,114 +23,161 @@ $(document).ready(function(){
 	var currentOperator = "";
 	var lastKey = 0;
 
+	//performs operations and returns the result
 	function regOperate(){
 		var result = 0;
+		console.log(regOne, regTwo);
 		switch(currentOperator){
 			case '/':
 				result = regOne / regTwo;
+				result = Math.round(result * 1000000000) / 1000000000;
 				break;
-			case 'X':
-				result = regOne * regTwo;
+
+			case 'x':
+				var temp1 = 0;
+				var temp2 = 0;
+				var correctionFactor = 1;
+				if((String(regOne).indexOf(".")!= -1)||(String(regTwo).indexOf(".")!= -1)){
+					console.log("here");
+					if(String(regOne).indexOf(".") != -1){
+						temp1 = String(regOne).length-String(regOne).indexOf(".");
+					}
+					if(String(regTwo).indexOf(".") != -1){
+						temp2 = String(regTwo).length-String(regTwo).indexOf(".");
+					}
+					correctionFactor = Math.pow(10, Math.max(temp1, temp2));
+				}
+				console.log(typeof regOne, typeof regTwo);
+				result = Number(regOne)*correctionFactor * Number(regTwo)*correctionFactor;
+				result /= Math.pow(correctionFactor, 2);
+				console.log(typeof regOne, typeof regTwo);
 				break;
 			case '+':
-				result = regOne + regTwo;
+				var temp1 = 0;
+				var temp2 = 0;
+				// if(temp1.indexOf)
+				var correctionFactor = 1;
+				if((String(regOne).indexOf(".")!= -1)||(String(regTwo).indexOf(".")!= -1)){
+					console.log("here");
+					if(String(regOne).indexOf(".") != -1){
+						temp1 = String(regOne).length-String(regOne).indexOf(".");
+					}
+					if(String(regTwo).indexOf(".") != -1){
+						temp2 = String(regTwo).length-String(regTwo).indexOf(".");
+					}
+					correctionFactor = Math.pow(10, Math.max(temp1, temp2));
+				}
+				console.log(typeof regOne, typeof regTwo);
+				result = Number(regOne)*correctionFactor + Number(regTwo)*correctionFactor;
+				result /= correctionFactor;
+				console.log(typeof regOne, typeof regTwo);
 				break;
 			case '-':
-				result = regOne - regTwo;
+				var temp1 = 0;
+				var temp2 = 0;
+				var correctionFactor = 1;
+				if((String(regOne).indexOf(".")!= -1)||(String(regTwo).indexOf(".")!= -1)){
+					console.log("here");
+					if(String(regOne).indexOf(".") != -1){
+						temp1 = String(regOne).length-String(regOne).indexOf(".");
+					}
+					if(String(regTwo).indexOf(".") != -1){
+						temp2 = String(regTwo).length-String(regTwo).indexOf(".");
+					}
+					correctionFactor = Math.pow(10, Math.max(temp1, temp2));
+				}
+				console.log(typeof regOne, typeof regTwo);
+				result = Number(regOne)*correctionFactor - Number(regTwo)*correctionFactor;
+				result /= correctionFactor;
+				console.log(typeof regOne, typeof regTwo);
 				break;
 			case '%':
 				result = regOne % regTwo;
+				result = Math.round(result * 1000000000) / 1000000000;
 				break;
 			default:
 				result = regTwo;
 		}
 		return result;
 	}
+	//Display functions just for code clarity
+	function displayReg(someArg){
+		$(".displayRegister").text(someArg);
+	}
+	function displayEnt(someArg){
+		$(".displayEntry").text(someArg);
+	}
 
+	//Function for number clicks and decimals
 	$(".num").click(function(){
+		//condition for the last key being "=" resets the registers if the next key is a number further ops require an operation to be clicked
 		if(lastKey == "="){
-			$(".displayRegister").text("");
+			//clear the registers
+			//clear the distplays
+			//clear current operator?
 			regOne = "";
+			regTwo = "";
+			currentOperator = "";
+			displayEnt("0");
+			displayReg("");
 		}
-		//if(displayVar == "0"){$(".displayEntry").text("");}
-		displayVar += $(this).text();
-		$(".displayEntry").text(displayVar);
-		//$(".displayRegister").text($(".displayRegister").text() + $(this).text());
-		regTwo = parseFloat(displayVar);
+		regTwo += $(this).text();
+		displayEnt(regTwo);
 		lastKey = $(this).text();
 	}); //num.click()
 
 	$(".operator").click(function(){
-
-		if($.isNumeric(lastKey)){
+		//store regOne in a temp variable since we'll need to display it after regOne has been changed if = was clicked.
+		var tempOne = regOne;
+		//only perform operations if last key was a number or period
+		if($.isNumeric(lastKey)||(lastKey == ".")){
+			regTwo = Number(regTwo);
+			// regOperate works with the last operator, since the operations are run before the new variable is set
 			regOne = regOperate();
 		}
-		currentOperator = $(this).text();
-		displayVar = regOne + " " + currentOperator + " ";
-		$(".displayRegister").text(displayVar);
-		displayVar = "";
-
-		if(lastKey == "="){displayVar = regOne;}
-		$(".displayEntry").text(displayVar);
+		//need two cases: one for equal and one for the rest
+		if($(this).text() == "="){
+			displayEnt(regOne);
+			//further condition for if lastKey was an operation
+			if($.isNumeric(lastKey)||(lastKey == ".")){
+				displayReg(tempOne + " " + currentOperator + " " + regTwo + " =");
+			}
+			else{
+				displayReg(tempOne + " =");
+			}
+			currentOperator = "=";
+		}
+		else{
+			displayEnt(0);
+			currentOperator = $(this).text();
+			displayReg(regOne + " " + currentOperator);
+		}
+		//clear reg 2 for further numeric entry
+		regTwo = "";
 		lastKey = $(this).text();
-		displayVar = "";
-		console.log(lastKey);
-
 	}); //operator.click()
 
+	$(".sign").click(function(){
+		regTwo *= -1;
+		displayEnt(regTwo);
+	});
 
-
-
-
-	$(".equal").click(function(){
-		console.log(regOne, currentOperator, regTwo);
-		var tempp = $(".displayEntry").text();
-		displayVar = regOne + " " + currentOperator + " " + regTwo + " = ";
-
-		console.log("regOne: "+regOne);
- 		if(lastKey == "X"){
- 			displayVar = regOne + " = ";}
- 		else if(lastKey == "/"){
- 			displayVar = regOne + " = ";}
- 		else if(lastKey == "+"){
- 			displayVar = regOne + " = ";}
- 		else if(lastKey == "-"){
- 			displayVar = regOne + " = ";}
- 		else if(lastKey == "%"){
- 			displayVar = regOne + " = ";}
- 		else if(lastKey == "="){
- 			displayVar = regOne + " = ";
- 			console.log("hey");
- 		}
-		// if(lastKey == "="){displayVar = tempp + " = ";}
-		$(".displayRegister").text(displayVar);
-		if($.isNumeric(lastKey)){
-			regOne = regOperate();
-		}
-		currentOperator = $(this).text();
-		displayVar = regOne == "" ? "0" : regOne;
-		$(".displayEntry").text(displayVar);
-		lastKey = "=";
-		displayVar = "";
-
-	}); //equal.click()
-
-
+	//All Clear clears all registers and returns to original display state
 	$(".ac").click(function(){
-		$(".displayEntry").text("0");
-		$(".displayRegister").text("");
-		displayVar = "";
 		regOne = "";
 		regTwo = "";
-		lastKey = 0;
 		currentOperator = "";
+		displayEnt("0");
+		displayReg("");
 	}); //ac.click()
 
+	//Clear Entry just clears register two and display entry
 	$(".ce").click(function(){
-		displayVar = "";
-		$(".displayEntry").text(displayVar);
+		regTwo = "";
+		displayEnt("0");
 	}); //ce.click()
 
+	//Function for keyboard entry
 	$(document).keypress(function(k){
 		switch(k.which){
 			case 13:
@@ -142,6 +191,9 @@ $(document).ready(function(){
 				break;
 			case 43:
 				$(".plus").trigger("click");
+				break;
+			case 44:
+				$(".sign").trigger("click");
 				break;
 			case 45:
 				$(".minus").trigger("click");
